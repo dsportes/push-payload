8674f95fd63700f820a8353a4da7327576c64c31
Daniel Sportes <daniel@sportes.fr>
2024-08-04 10:32:47 +0200

    async

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -2,23 +2,18 @@
 const broadcast = new BroadcastChannel('channel-123');
 
 // Register event listener for the 'push' event.
-self.addEventListener('push', function(event) {
+self.addEventListener('push', (event) => {
   // Retrieve the textual payload from event.data (a PushMessageData object).
   // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
   // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
-  const payload = event.data ? event.data.text() : 'no payload';
+  const payload = event.data ? event.data.text() : 'no payload'
 
   //send message
-  broadcast.postMessage({ type: 'MSG_ID', payload: payload});
+  broadcast.postMessage({ type: 'MSG_ID', payload: payload})
 
-  /*
-  // Keep the service worker alive until the notification is created.
-  event.waitUntil(
-    // Show a notification with title 'ServiceWorker Cookbook' and use the payload
-    // as the body.
-    self.registration.showNotification('ServiceWorker Cookbook', {
-      body: payload,
-    })
-  );
+  /* Keep the service worker alive until the notification is created
+  event.waitUntil( // Show a notification with a title and the payload as the body.
+    self.registration.showNotification('ServiceWorker Cookbook', { body: payload })
+  )
   */
-});
+})
--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var app = express();
-const webPush = require("web-push");
+const express = require('express')
+const bodyParser = require('body-parser')
+const webPush = require("web-push")
 
-const route = '/'
+const app = express()
 
 /*
 const keys = webPush.generateVAPIDKeys();
@@ -19,115 +17,69 @@ webPush.setVapidDetails(
   "https://example.com/",
   VAPID_PUBLIC_KEY,
   VAPID_PRIVATE_KEY
-);
+)
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use(function maskDemoIndexes(req, res, next) {
-  // Adding this redirect to simplify caching a recipe page,
-  // essentially so we don't have to cache "/" and "/index.html"
-  // So: "recipe/index.html" -> "recipe/" , "index.html?123" -> "?123"
-  if (/\/(.*)\/index\.html\??(.*)$/.test(req.url)) {
-    return res.redirect(req.url.replace('index.html', ''));
-  }
-  return next();
-});
-
-app.use(function setHomepageCanonical(req, res, next) {
-  // Better for canonical URL, "index.html" is ugly 
-  if(req.url === '/index.html') {
-    return res.redirect(301, '/');
-  }
-  return next();
-});
-
-app.use(function forceLiveDomain(req, res, next) {
-  // Don't allow user to hit Heroku now that we have a domain
-  var host = req.get('Host');
-
-  if (host === 'serviceworker-cookbook.herokuapp.com') {
-    return res.redirect(301, 'https://serviceworke.rs');
-  }
-  return next();
-});
-
-app.use(function forceSSL(req, res, next) {
-  var host = req.get('Host');
-  var localhost = 'localhost';
+app.use((req, res, next) => { // force SSL
+  const host = req.get('Host')
+  const localhost = 'localhost'
 
   if (host.substring(0, localhost.length) !== localhost) {
     // https://developer.mozilla.org/en-US/docs/Web/Security/HTTP_strict_transport_security
-    res.header('Strict-Transport-Security', 'max-age=15768000');
+    res.header('Strict-Transport-Security', 'max-age=15768000')
     // https://github.com/rangle/force-ssl-heroku/blob/master/force-ssl-heroku.js
     if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect('https://' + host + req.url);
+      return res.redirect('https://' + host + req.url)
     }
   }
-  return next();
-});
+  return next()
+})
 
-app.use(function corsify(req, res, next) {
+app.use((req, res, next) => {
   // http://enable-cors.org/server_expressjs.html
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept')
   next();
-});
+})
 
-app.use(function setServiceWorkerHeader(req, res, next) {
+app.use((req, res, next) => {
   // https://github.com/mozilla/serviceworker-cookbook/issues/201
-  var file = req.url.split('/').pop();
+  const file = req.url.split('/').pop()
   if (file === 'service-worker.js' || file === 'worker.js') {
     res.header('Cache-control', 'public, max-age=0');
   }
-  next();
-});
+  next()
+})
 
-if (!fs.existsSync('./dist')) {
-  throw new Error('Missing `dist` folder, execute `npm run build` first.');
-}
 app.use(express.static('./dist'));
 
-app.get("/vapidPublicKey", function (req, res) {
-  res.send(VAPID_PUBLIC_KEY);
-});
+// app.get("/vapidPublicKey", (req, res) => { res.send(VAPID_PUBLIC_KEY) })
 
 app.post("/register", function (req, res) {
   // A real world application would store the subscription info.
-  res.sendStatus(201);
+  const subscription = req.body.subscription
+  res.sendStatus(201)
 });
 
 app.post("/sendNotification", function (req, res) {
-  const subscription = req.body.subscription;
-  const payload = req.body.payload;
-  const options = {
-    TTL: req.body.ttl,
-  };
+  const subscription = req.body.subscription
+  const payload = req.body.payload
+  const options = { TTL: req.body.ttl, }
 
-  setTimeout(function () {
-    webPush
-      .sendNotification(subscription, payload, options)
-      .then(function () {
-        res.sendStatus(201);
-      })
-      .catch(function (error) {
-        console.log(error);
-        res.sendStatus(500);
-      });
-  }, req.body.delay * 1000);
-});
-
-
-var port = process.env.PORT || 3003;
-var ready = new Promise(function willListen(resolve, reject) {
-  app.listen(port, function didListen(err) {
-    if (err) {
-      reject(err);
-      return;
+  setTimeout(async () => {
+    try {
+      await webPush.sendNotification(subscription, payload, options)
+      res.sendStatus(201)
+    } catch (error) {
+      console.log(error)
+      res.sendStatus(500)
     }
-    console.log('app.listen on http://localhost:%d', port);
-    resolve();
-  });
-});
+  }, req.body.delay * 1000)
+})
 
-exports.ready = ready;
-exports.app = app;
+const port = process.env.PORT || 3003
+app.listen(port, (err) => {
+  if (err) console.error('server.js : HTTP error = ' + e.message + '\n' + e.stack)
+  else console.log('HTTP_SERVER écoute [' + port +']')
+})
